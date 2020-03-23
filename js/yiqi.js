b9ac376f0b58ac54adb17e5c2253609c4a283e6e
liuyang111bug <62432943+liuyang111bug@users.noreply.github.com>
2020-03-23 18:47:46 +0800

    Add files via upload

---
+++ js/yiqi.js
@@ -0,0 +1,34 @@
+﻿// JavaScript Document
+var Dianji=0;
+window.onload=function(){
+	var buhao = document.getElementById("buhao");
+	var hao = document.getElementById("hao");
+	buhao.onclick=function(){
+		Dianji++;
+		if(Dianji==1){
+	   			alert("来登封吧");
+	   }else if(Dianji==2){
+		   		alert("吃火锅");
+		   		
+	   }else if(Dianji==3){
+		   		alert("看电影");
+		   		
+	   }else if(Dianji==4){
+		   		alert("登嵩山");
+		   		
+	  }else if(Dianji==5){
+		   		alert("逛街");
+	  }else if(Dianji==6){
+		   		alert("好不好");
+	  }else if(Dianji==7){
+		   		alert("工");
+		  		Dianji=1;
+	  }
+
+	}
+	hao.onclick=function(){
+		alert("那快点来吧我去接你");
+	}
+	
+
+}
